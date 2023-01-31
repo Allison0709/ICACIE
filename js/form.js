9a8a3eecdf8b8092fefe52ab75c223073570f0e0
Allison0709 <79126275+Allison0709@users.noreply.github.com>
2023-01-31 19:57:29 +0000

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,10 @@
+// matthais example
+document.querySelector("#survey-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const arr = [...data];
+    console.table(arr);
+    console.log(data.getAll("langs"));
+    console.dir(Object.fromEntries(arr));
+  });
+  
